45b951eede3d5e29351b899ebf932c45532947e8
Alberto-Gomez-97 <120439839+Alberto-Gomez-97@users.noreply.github.com>
2023-08-06 18:44:40 -0600

    Add files via upload

---
+++ Switch_udy/Switch_udy/Program.cs
@@ -0,0 +1,20 @@
+﻿// Switch
+var lugar = 4;
+switch (lugar)
+{
+    case 1:
+        Console.WriteLine("Quedaste en primer lugar");
+        break;
+    case 2:
+        Console.WriteLine("Quedaste en segundo lugar");
+        break;
+    case 3:
+        Console.WriteLine("Quedaste en tercer lugar");
+        break;
+        case 4:
+        case 5:
+            Console.WriteLine("Casi lo lograste");
+        break;
+    default: Console.WriteLine("Mejor suerte a la proxima");
+        break;
+}
